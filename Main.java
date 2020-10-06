49456ed3b73a8bdb098d8d957110a0086fa734fd
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-06 13:12:07 +0000

    Prints numbers on one line with commas

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks user for integer and prints all numbers up to that one on same line with commas
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to enter in a positive integer
+    System.out.println("Please enter a positive integer");
+    // create variable for user input
+    int posInt = input.nextInt();
+
+    for(int count = 1; count <= posInt; count++){
+      // print to user numbers with commas
+      if(count == posInt){
+        System.out.print(count);
+      }else{
+        System.out.print(count + ",");
+      }
+      
+    }
     
   }
 }
